4afc75763b0b7cd1369cf709494d11e5a682cb5c
Dan <dan.pandolfo@gmail.com>
2021-09-24 17:24:05 -0400

    created the moments comparison for colours as well as wrote out the README

--- README.md
+++ README.md
@@ -1,46 +1,27 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Create a simple calendar application that allows the user to save events for each hour of the day.
 
-The starter code uses the [Moment.js](https://momentjs.com/) library to work with date and time, but feel free to use a different JavaScript solution to handle this functionality since Moment.js is considered a "legacy" project. Learn more about these other solutions in the [Moment.js project status page.](https://momentjs.com/docs/#/-project-status/)
 
-## User Story
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+## process
 
-## Acceptance Criteria
+* wrote the HTML to display everything properly
+* added js and jQuery to write out the functions to capture inputs
+* wrote the localStorage functions to store the data 
+* wrote the localStorage getitems functions to pull the data on page refresh
+* used moments to put the time display in the header
+* used moments to create if else if and else statment to determin time in each hour block to display colour based on past present or future
+* detailed out the README
 
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+## Links
+GitHub: https://github.com/ToiletTakos/work-day-schedule
 
-The following animation demonstrates the application functionality:
+Deployed app(pages): https://toilettakos.github.io/work-day-schedule/
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+## Screenshots
+![display area testing](./assets/images/display-area-html.png)
+![localStorage set up](./assets/images/localStorage-test.png)
+![proof item is in localStorage](./assets/images/item-displayed-in-localStorage.png)
+![Final display with colours for time comparison](./assets/images/colouring-test-with-moments.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,89 +8,241 @@ var time = moment();
 $(currentDay).text(moment(time).format('dddd, MMMM Do h:mm:ss a'));
 
 //set all inputs into localStorage
+//nine
 function saveNine() {
     var nine = document.getElementById("nine").value;
     localStorage.setItem("timeNine", nine);
 }
+//ten
 function saveTen() {
     var ten = document.getElementById("ten").value;
     localStorage.setItem("timeTen", ten);
 }
+// eleven
 function saveEleven() {
     var eleven = document.getElementById("eleven").value;
     localStorage.setItem("timeEleven", eleven);
 }
+// twleve
 function saveTwelve() {
     var twelve = document.getElementById("twelve").value;
     localStorage.setItem("timeTwelve", twelve);
 }
+//one
 function saveOne() {
     var one = document.getElementById("one").value;
     localStorage.setItem("timeOne", one);
 }
+// two
 function saveTwo() {
     var two = document.getElementById("two").value;
     localStorage.setItem("timeTwo", two);
 }
+// three
 function saveThree() {
     var three = document.getElementById("three").value;
     localStorage.setItem("timeThree", three);
 }
+// four
 function saveFour() {
     var four = document.getElementById("four").value;
     localStorage.setItem("timeFour", four);
 }
+// five
 function saveFive() {
     var five = document.getElementById("five").value;
     localStorage.setItem("timeFive", five);
 }
 
 //Function to recall anything saved in localStorage
+
+//nine
 function getNine(){
     var returnNine = localStorage.getItem("timeNine");
 
     document.getElementById('nine').value = returnNine;
-    document.getElementById('nine').className("present")
+
+    var nineColour = document.getElementById("nine")
+    
+
+    var time = moment().set("hour", 9);
+    if(moment().isBefore(time)) {
+        $(nineColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(nineColour).addClass("present");
+    }
+    else {
+        $(nineColour).addClass("past")
+    }
 }
+
+//ten
 function getTen(){
     var returnTen = localStorage.getItem("timeTen");
 
     document.getElementById('ten').value = returnTen;
+
+    var tenColour = document.getElementById("ten")
+    
+
+    var time = moment().set("hour", 10);
+    if(moment().isBefore(time)) {
+        $(tenColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(tenColour).addClass("present");
+    }
+    else {
+        $(tenColour).addClass("past")
+    }
 }
+
+// eleven
 function getEleven(){
     var returnEleven = localStorage.getItem("timeEleven");
 
     document.getElementById('eleven').value = returnEleven;
+
+    var elevenColour = document.getElementById("eleven")
+    
+
+    var time = moment().set("hour", 11);
+    if(moment().isBefore(time)) {
+        $(elevenColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(elevenColour).addClass("present");
+    }
+    else {
+        $(elevenColour).addClass("past")
+    }
 }
+
+// twelve
 function getTwelve(){
     var returnTwelve = localStorage.getItem("timeTwelve");
 
     document.getElementById('twelve').value = returnTwelve;
+
+    var twelveColour = document.getElementById("twelve")
+    
+
+    var time = moment().set("hour", 12);
+    if(moment().isBefore(time)) {
+        $(twelveColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(twelveColour).addClass("present");
+    }
+    else {
+        $(twelveColour).addClass("past")
+    }
 }
+
+//one
 function getOne(){
     var returnOne = localStorage.getItem("timeOne");
 
     document.getElementById('one').value = returnOne;
+
+    var oneColour = document.getElementById("one")
+    
+
+    var time = moment().set("hour", 13);
+    if(moment().isBefore(time)) {
+        $(oneColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(oneColour).addClass("present");
+    }
+    else {
+        $(oneColour).addClass("past")
+    }
 }
+
+// two
 function getTwo(){
     var returnTwo = localStorage.getItem("timeTwo");
 
     document.getElementById('two').value = returnTwo;
+
+    var twoColour = document.getElementById("two")
+    
+
+    var time = moment().set("hour", 14);
+    if(moment().isBefore(time)) {
+        $(twoColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(twoColour).addClass("present");
+    }
+    else {
+        $(twoColour).addClass("past")
+    }
 }
+
+// three
 function getThree(){
     var returnThree = localStorage.getItem("timeThree");
 
     document.getElementById('three').value = returnThree;
+
+    var threeColour = document.getElementById("three")
+    
+
+    var time = moment().set("hour", 15);
+    if(moment().isBefore(time)) {
+        $(threeColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(threeColour).addClass("present");
+    }
+    else {
+        $(threeColour).addClass("past")
+    }
 }
+
+// four
 function getFour(){
     var returnFour = localStorage.getItem("timeFour");
 
     document.getElementById('four').value = returnFour;
+
+    var fourColour = document.getElementById("four")
+    
+
+    var time = moment().set("hour", 16);
+    if(moment().isBefore(time)) {
+        $(fourColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(fourColour).addClass("present");
+    }
+    else {
+        $(fourColour).addClass("past")
+    }
 }
+
+// five
 function getFive(){
     var returnFive = localStorage.getItem("timeFive");
 
     document.getElementById('five').value = returnFive;
+
+    var fiveColour = document.getElementById("five")
+    
+
+    var time = moment().set("hour", 17);
+    if(moment().isBefore(time)) {
+        $(fiveColour).addClass("future");
+    }
+    else if(moment().isSame(time)) {
+        $(fiveColour).addClass("present");
+    }
+    else {
+        $(fiveSColour).addClass("past")
+    }
 }
 
 // Display what was stored in localStorage
